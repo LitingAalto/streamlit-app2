e9cf96ec98757150b5b41864a35ed6cdc3125544
liting <cailiting1993@gmail.com>
2022-09-30 10:38:34 +0300

    Update sos.py

--- sos.py
+++ sos.py
@@ -21,7 +21,6 @@ headers = {
     'sec-ch-ua-platform': '"Windows"',
 }
 
-
 class TrendReq(UTrendReq):
     def _get_data(self, url, method=GET_METHOD, trim_chars=0, **kwargs):
         return super()._get_data(url, method=GET_METHOD, trim_chars=trim_chars, headers=headers, **kwargs)
