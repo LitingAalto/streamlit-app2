ad95aaf166d536dacab640dfd059079687296c5f
liting <cailiting1993@gmail.com>
2022-10-30 16:41:27 +0200

    Update sos.py

--- sos.py
+++ sos.py
@@ -136,7 +136,7 @@ def download_files(ind, keyword, t= 10):
                       'profile.default_content_settings.popups' : 0}
 
     chrome_options.add_experimental_option('prefs', download_prefs)
-#     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--window-size=1920x1080')
     url = f'https://trends.google.com/trends/explore?date={str(start)}%20{str(end)}&geo=FI&q=' + keyword
     # Start up browser
